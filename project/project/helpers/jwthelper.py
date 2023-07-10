d55f83861f6a813208feb8c13b63cc6f6765bdec
“artuino0” <37974162+artuino0@users.noreply.github.com>
2023-07-10 11:20:55 -0600

    Last commit

--- project/project/helpers/jwthelper.py
+++ project/project/helpers/jwthelper.py
@@ -8,9 +8,7 @@ ALGORITHM = "HS256"
 
 def create_jwt_token(user_id: int) -> str:
     payload = {'user_id': user_id, 'exp': datetime.utcnow() + timedelta(minutes=120)}
-    
     token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
-
     return token
 
 def decode_jwt_token(token: str) -> dict:
